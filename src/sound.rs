334177f37aa97b569a5b440f7acde96ae38d603d
Nobuo Saeki <nobuo.saeki@gmail.com>
2023-08-28 09:14:29 +0900

    Fix an argument type for AudioBufferSourceNode.set_buffer()

--- src/sound.rs
+++ src/sound.rs
@@ -1,5 +1,5 @@
 use anyhow::{anyhow, Result};
-use web_sys::{AudioBufferSourceNode, AudioContext, AudioDestinationNode, AudioNode};
+use web_sys::{AudioBuffer, AudioBufferSourceNode, AudioContext, AudioDestinationNode, AudioNode};
 
 pub fn create_audio_context() -> Result<AudioContext> {
     AudioContext::new().map_err(|err| anyhow!("Could not create audio context: {:#?}", err))
@@ -19,7 +19,7 @@ fn connect_with_audio_node(
         .map_err(|err| anyhow!("Error connecting audio source to destination {:#?}", err))
 }
 
-pub fn play_sound(ctx: &AudioContext, buffer: &AudioBufferSourceNode) -> Result<()> {
+pub fn play_sound(ctx: &AudioContext, buffer: &AudioBuffer) -> Result<()> {
     let track_source = create_buffer_source(ctx)?;
     track_source.set_buffer(Some(&buffer));
     connect_with_audio_node(&track_source, &ctx.destination())?;
